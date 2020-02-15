459664748a623b99301f646f07198b914575e145
yonghochoi <yongho1037@gmail.com>
2020-02-16 03:31:57 +0900

    jwt-go v4로 변경하여 go module 적용

--- pkg/token/jwt.go
+++ pkg/token/jwt.go
@@ -1,13 +1,13 @@
 package token
 
 import (
-	"github.com/dgrijalva/jwt-go"
+	jwtv4 "github.com/dgrijalva/jwt-go/v4"
 	"time"
 )
 
 type Claims struct {
 	Name string
-	jwt.StandardClaims
+	jwtv4.StandardClaims
 }
 
 const SecretKey = "secret key"
@@ -16,13 +16,13 @@ func MakeJWT(name string) (string, error) {
 	// 사용자 클라임 정의
 	claims := &Claims{
 		Name: name,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(5 * time.Minute).Unix(),
+		StandardClaims: jwtv4.StandardClaims{
+			ExpiresAt: &jwtv4.Time{Time: time.Now().Add(5 * time.Minute)},
 		},
 	}
 
 	// 토큰 생성 에 필요한 Header 정보와 Payload에 들어갈 Cliam 전달
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token := jwtv4.NewWithClaims(jwtv4.SigningMethodHS256, claims)
 
 	// Header와 Payload 값 기반으로 Signing
 	// Signing 된 결과도 base64로 인코딩 되어 JWT에 dot(.) 구분자로 포함됨
